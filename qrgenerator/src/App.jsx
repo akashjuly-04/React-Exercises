d5a95fd5fdc04cce5eeec7bfe4c68e3ed94deb11
akashjuly-04 <akashkumarrajappan@gmail.com>
2025-03-11 13:04:10 +0530

    "QR Generator Done"

---
+++ qrgenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import QRGenerator from './QRGenerator'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <QRGenerator/>
+    </>
+  )
+}
+
+export default App
